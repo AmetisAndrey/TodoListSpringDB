88a582d789f267a189a52c332f7e0897ea10c2ef
AmetisAndrey <Ametisgoga70777@mail.ru>
2025-11-27 20:21:14 +0300

    feature/auth add auth

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,23 @@ CREATE TABLE IF NOT EXISTS tasks (
     created_at timestamp not null,
     updated_at timestamp not null
     );
+
+
+CREATE TABLE IF NOT EXISTS authorities(
+    username varchar(50) not null,
+    password varchar(50) not null,
+    constraint fk_authorities_users
+        foreign KEY (username) references users(username)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS ix_auth_username
+    on authorities (username, authority);
+
+
+INSERT INTO users (username, password, enabled)
+VALUES ('admin', '{noop}admin', TRUE)
+    ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO authorities (username, authority)
+VALUES ('admin', 'ROLE_ADMIN')
+    ON CONFLICT (username, authority) DO NOTHING;
